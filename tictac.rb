e28a340599a90747bcddcd69ed51a9ca855c9227
mattrose570 <mattrose570@gmail.com>
2023-06-07 19:41:15 -0500

    Class definition o Added error handling in the player 1 & 2 data entry o took the while loop out of the player's turn methods

--- tictac.rb
+++ tictac.rb
@@ -20,6 +20,7 @@ class TicTac
         @winner = false
         @draw = false
         @turn = 0
+        @moves = []
         
     end
 
@@ -53,11 +54,18 @@ class TicTac
         
         elsif @the_board[2][0..2].all? {|num| num == "x"}
             return true
+
+        elsif @the_board[0][0] == "x" && @the_board[1][0] == "x" && @the_board[2][0] == "x"
+            return true
+        elsif @the_board[0][1] == "x" && @the_board[1][1] == "x" && @the_board[2][1] == "x"
+            return true
+            elsif @the_board[0][2] == "x" && @the_board[1][2] == "x" && @the_board[2][2] == "x"
+                return true
         
-        elsif @the_board[0][0] == 1 && @the_board[1][1] == "x" && @the_board[2][2] == "x"
+        elsif @the_board[0][0] == "x" && @the_board[1][1] == "x" && @the_board[2][2] == "x"
             return true
 
-        elsif @the_board[0][2] == 1 && @the_board[1][1] == "x" && @the_board[2][0] == "x"
+        elsif @the_board[0][2] == "x" && @the_board[1][1] == "x" && @the_board[2][0] == "x"
             return true
 
         else   
@@ -107,42 +115,75 @@ class TicTac
     end
 
     def player1_turn
+        puts("Your symbol is X")
         puts "#{@player1.name}'s turn. Enter the number of the tile you'd like to claim"
+        
         userChoice = gets.chomp.to_i
-        if userChoice == 1 || userChoice == 2 || userChoice == 3
         
-            @the_board[0][userChoice - 1] = "x"
+        if (userChoice >= 1 && userChoice <= 9) && !@moves.include?(userChoice)
+            if userChoice == 1 || userChoice == 2 || userChoice == 3
+                @the_board[0][userChoice - 1] = "x"
+            
+            elsif userChoice == 4 || userChoice == 5 || userChoice == 6
+                @the_board[1][userChoice - 4] = "x"
+                
+            elsif userChoice == 7 || userChoice == 8 || userChoice == 9
+                @the_board[2][userChoice - 7] = "x"
+            end
+            
             @turn += 1
+            @moves << userChoice
         
-        elsif userChoice == 4 || userChoice == 5 || userChoice == 6
-            @the_board[1][userChoice - 4] = "x"
-            @turn += 1
+        else 
+            if userChoice < 1 || userChoice > 9
+                puts("You have entered an invalid value.")
+                puts("Please try again. Enter an integer from 1 through 9")
+                printBoard
+                return player1_turn
 
-        elsif userChoice == 7 || userChoice == 8 || userChoice == 9
-            @the_board[2][userChoice - 7] = "x"
-            @turn += 1
-
-        end
-        
+            elsif @moves.include?(userChoice)
+                puts("That spot is already taken")
+                puts("Enter an available spot")
+                printBoard
+                return player1_turn
+            end
+        end        
     end
 
     def player2_turn
-        puts("Your symbol is the O")
+        puts("Your symbol is O")
         puts "#{@player2.name}'s turn. Enter the number of the tile you'd like to claim"
-        userChoice = gets.chomp.to_i
-        if userChoice == 1 || userChoice == 2 || userChoice == 3
-            @the_board[0][userChoice - 1] = "o"
-            @turn += 1
         
-        elsif userChoice == 4 || userChoice == 5 || userChoice == 6
-            @the_board[1][userChoice - 4] = "o"
+        userChoice = gets.chomp.to_i
+        
+        if (userChoice >= 1 && userChoice <= 9) && !@moves.include?(userChoice)
+            if userChoice == 1 || userChoice == 2 || userChoice == 3
+                @the_board[0][userChoice - 1] = "o"
+            
+            elsif userChoice == 4 || userChoice == 5 || userChoice == 6
+                @the_board[1][userChoice - 4] = "o"
+                
+            elsif userChoice == 7 || userChoice == 8 || userChoice == 9
+                @the_board[2][userChoice - 7] = "o"
+            end
+            
             @turn += 1
+            @moves << userChoice
+        
+        else 
+            if userChoice < 1 || userChoice > 9
+                puts("You have entered an invalid value.")
+                puts("Please try again. Enter an integer from 1 through 9")
+                printBoard
+                return player2_turn
 
-        elsif userChoice == 7 || userChoice == 8 || userChoice == 9
-            @the_board[2][userChoice - 7] = "o"
-            @turn += 1
-
-        end
+            elsif @moves.include?(userChoice)
+                puts("That spot is already taken")
+                puts("Enter an available spot")
+                printBoard
+                return player2_turn
+            end
+        end 
     end
 
     def get_winner?
@@ -159,10 +200,10 @@ class TicTac
 
     def input_name
         print("Please enter the name of player 1: ")
-        @player1.name = gets.chomp
+        @player1.name = gets.chomp.capitalize 
 
         print("Please enter the name of player 2: ")
-        @player2.name = gets.chomp
+        @player2.name = gets.chomp.capitalize
 
     end
 
@@ -170,6 +211,7 @@ class TicTac
         
         # User input the player's names
         input_name
+        
         printBoard
 
         # Loop  winner condition
@@ -186,8 +228,7 @@ class TicTac
             if get_draw?
                 break
             end
-        
-        
+
             # Game goes on
             printBoard
 
@@ -209,11 +250,6 @@ class TicTac
         puts("It's a draw.")
         continue_game? ? game_loop : exit
     end
-        
-        
-        
-
-        
     # End of game_loop
     
 
@@ -235,9 +271,12 @@ class TicTac
 
     def continue_game?
         print("Would you like to play another game? (y or n) ")
-        decision = gets.chomp
-
-        if decision == "y"
+        decision = gets.chomp.to_s.downcase
+        if decision != "y" && decision != "n"
+            puts("Please enter either 'y' for yes or 'n' for no")
+            continue_game?
+        
+        elsif decision == "y"
             return true
         else 
             return false
